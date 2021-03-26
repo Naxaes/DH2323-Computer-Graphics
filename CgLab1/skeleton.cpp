65a860866d5a1defd2807acfa339cc93975ebbf9
tjena1 <64324604+tjena1@users.noreply.github.com>
2021-03-26 21:03:47 +0100

    Update skeleton.cpp
    Both rainbow and starfield complete

--- CgLab1/skeleton.cpp
+++ CgLab1/skeleton.cpp
@@ -19,15 +19,17 @@ using glm::vec3;
 // --------------------------------------------------------
 // GLOBAL VARIABLES
 
-const int SCREEN_WIDTH  = 640;
+const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+int t;
+bool isRainbow = false;
 
 // --------------------------------------------------------
 // FUNCTION DECLARATIONS
 
 void DrawRainbow(SDL_Surface* screen);
 void DrawStarField(SDL_Surface* screen, const std::vector<vec3>& stars);
-void UpdateStarField(std::vector<vec3>& stars,  vec3 velocity);
+void UpdateStarField(std::vector<vec3>& stars, vec3 velocity);
 
 // --------------------------------------------------------
 // FUNCTION DEFINITIONS
@@ -70,6 +72,7 @@ vec3 Random() {
 int main(int argc, char* argv[])
 {
     std::vector<vec3> stars(1000);
+    t = SDL_GetTicks();
     for (int i = 0; i < stars.size(); ++i) {
         stars[i] = Random();
     }
@@ -77,8 +80,22 @@ int main(int argc, char* argv[])
     SDL_Surface* screen = InitializeSDL(SCREEN_WIDTH, SCREEN_HEIGHT);
     while (NoQuitMessageSDL())
     {
-        UpdateStarField(stars, vec3(0, 0, -0.01));
-        DrawStarField(screen, stars);
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_SPACE) {
+                    isRainbow != isRainbow;
+                }
+            }
+        }
+        if (isRainbow) {
+            DrawRainbow(screen);
+        }
+        else {
+            UpdateStarField(stars, vec3(0, 0, 0.0001));
+            DrawStarField(screen, stars);
+        }
+        
 
         // DrawRainbow(screen);
 
@@ -87,14 +104,18 @@ int main(int argc, char* argv[])
 
         SDL_UpdateRect(screen, 0, 0, 0, 0);
     }
-    SDL_SaveBMP( screen, "../" __TIME__ "screenshot.bmp" );
+    SDL_SaveBMP(screen, "../" __TIME__ "screenshot.bmp");
     return 0;
 }
 
 void UpdateStarField(std::vector<vec3>& stars, vec3 velocity) {
+
+    int t2 = SDL_GetTicks();
+    float dt = float(t2 - t);
+    t = t2;
     for (int i = 0; i < stars.size(); ++i) {
 
-        stars[i] += velocity;
+        stars[i] += stars[i] - (velocity * dt);
 
         if (stars[i].z <= 0)
             stars[i].z += 1;
@@ -104,18 +125,20 @@ void UpdateStarField(std::vector<vec3>& stars, vec3 velocity) {
 }
 
 void DrawStarField(SDL_Surface* screen, const std::vector<vec3>& stars) {
-    for (int y = 0; y < SCREEN_HEIGHT; ++y)
+    /*for (int y = 0; y < SCREEN_HEIGHT; ++y)
         for (int x = 0; x < SCREEN_WIDTH; ++x)
-            PutPixelSDL(screen, x, y, vec3(0.0, 0.0, 0.0));
+            PutPixelSDL(screen, x, y, vec3(0.0, 0.0, 0.0));*/
 
-    float f = SCREEN_HEIGHT / 2.0f;
+    SDL_FillRect(screen, 0, 0); //Given in the instructions
+
+    float f = SCREEN_HEIGHT / 2.0f; //Given in the instructions
     for (int i = 0; i < stars.size(); ++i) {
         auto position = stars[i];
 
-        auto u = int(f * position.x / position.z + SCREEN_WIDTH  / 2.0f);
+        auto u = int(f * position.x / position.z + SCREEN_WIDTH / 2.0f);
         auto v = int(f * position.y / position.z + SCREEN_HEIGHT / 2.0f);
-
-        PutPixelSDL(screen, u, v, vec3(1.0, 1.0, 1.0));
+        vec3 color = 0.2f * vec3(1, 1, 1) / (stars[i].z * stars[i].z); //Given in the instructions
+        PutPixelSDL(screen, u, v, color);
     }
 }
 
@@ -127,7 +150,7 @@ void DrawRainbow(SDL_Surface* screen)
     vec3 GREEN(0, 1, 0);  // green
     vec3 YELLOW(1, 1, 0); // yellow
 
-    auto left  = Interpolate(RED, GREEN, SCREEN_HEIGHT);
+    auto left = Interpolate(RED, GREEN, SCREEN_HEIGHT);
     auto right = Interpolate(BLUE, YELLOW, SCREEN_HEIGHT);
 
     for (int y = 0; y < SCREEN_HEIGHT; ++y)
