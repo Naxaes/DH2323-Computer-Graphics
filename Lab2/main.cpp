4f787e3a4c2e63ddd5c621c0f3ed50f490f5eee9
tjena1 <64324604+tjena1@users.noreply.github.com>
2021-04-18 22:34:01 +0200

    Update main.cpp
    Final version

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -40,11 +40,15 @@ struct Camera {
 // --------------------------------------------------------
 // GLOBAL VARIABLES
 
-const int SCREEN_WIDTH  = 100;
+const int SCREEN_WIDTH = 100;
 const int SCREEN_HEIGHT = 100;
 
-vec3 light_position = vec3(0.0f, 1.0f, 0.0f);
-vec3 light_color    = 14.0f * vec3( 1, 1, 1 );
+vec3 light_position1 = vec3(0, -0.8, 0.8);
+vec3 light_position = light_position1 /glm::length(light_position1);
+
+vec3 light_color = 14.0f * vec3(1, 1, 1);
+
+vec3 indirectLight = 0.5f * vec3(1, 1, 1);
 
 
 // --------------------------------------------------------
@@ -53,14 +57,14 @@ vec3 light_color    = 14.0f * vec3( 1, 1, 1 );
 void Update(float dt, Camera& camera);
 void Draw(Window& window, const Camera& camera, const vector<Triangle>& triangles);
 bool ClosestIntersection(vec3 start, vec3 direction, const vector<Triangle>& triangles, Intersection& closest_intersection);
-vec3 DirectLight(const Triangle& triangle, const Intersection& intersection);
+vec3 DirectLight(const Triangle& triangle, vector<Triangle> triangles, const Intersection& intersection);
 
 
 // --------------------------------------------------------
 // FUNCTION DEFINITIONS
 
 mat3 rotation_x(float theta) {
-    mat3 rotation {
+    mat3 rotation{
         {1, 0, 0},
         {0, cos(theta), -sin(theta)},
         {0, sin(theta),  cos(theta)}
@@ -68,7 +72,7 @@ mat3 rotation_x(float theta) {
     return rotation;
 }
 mat3 rotation_y(float theta) {
-    mat3 rotation {
+    mat3 rotation{
         { cos(theta), 0, sin(theta)},
         {      0,     1,     0     },
         {-sin(theta), 0, cos(theta)}
@@ -76,7 +80,7 @@ mat3 rotation_y(float theta) {
     return rotation;
 }
 mat3 rotation_z(float theta) {
-    mat3 rotation {
+    mat3 rotation{
         {cos(theta), -sin(theta), 0},
         {sin(theta),  cos(theta), 0},
         {     0,          0,      1},
@@ -97,8 +101,8 @@ int main(int argc, char* argv[])
     float F = H / 2.0f;
 
     Window window = Window::Create("Lab1", SCREEN_WIDTH, SCREEN_HEIGHT);
-    Clock  clock  = Clock();
-    Camera camera = Camera {
+    Clock  clock = Clock();
+    Camera camera = Camera{
         vec3(0.0f, 0.0f, 2.0f),  // position
         vec3(0.0f, 0.0f, 2.0f),  // velocity
         vec3(1.0f, 0.0f, 0.0f),  // right
@@ -141,24 +145,26 @@ void Update(float dt, Camera& camera)
 {
     std::cout << "Render time: " << dt << " s." << std::endl;
 
-	const Uint8* key_state = SDL_GetKeyboardState(nullptr);
+    const Uint8* key_state = SDL_GetKeyboardState(nullptr);
 
-	if (key_state[SDL_SCANCODE_UP])     { light_position.z -= 1.0f * dt; }
-	if (key_state[SDL_SCANCODE_DOWN])   { light_position.z += 1.0f * dt; }
-	if (key_state[SDL_SCANCODE_LEFT])   { light_position.x -= 1.0f * dt; }
-	if (key_state[SDL_SCANCODE_RIGHT])  { light_position.x += 1.0f * dt; }
-	if (key_state[SDL_SCANCODE_RSHIFT]) { std::cout << "Pressing RSHIFT" << std::endl; }
-	if (key_state[SDL_SCANCODE_RCTRL])  { std::cout << "Pressing RCTRL"	 << std::endl; }
-	if (key_state[SDL_SCANCODE_W])      { camera.position.z -= 1.0f * dt; }
-	if (key_state[SDL_SCANCODE_S])      { camera.position.z += 1.0f * dt; }
-	if (key_state[SDL_SCANCODE_D])      { camera.position.x += 1.0f * dt; }
-	if (key_state[SDL_SCANCODE_A])      { camera.position.x -= 1.0f * dt; }
-	if (key_state[SDL_SCANCODE_E])      { camera.yaw += float(M_PI / 4.0) * dt; }
-	if (key_state[SDL_SCANCODE_Q])      { camera.yaw -= float(M_PI / 4.0) * dt; }
+    if (key_state[SDL_SCANCODE_UP]) { light_position.z -= 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_DOWN]) { light_position.z += 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_LEFT]) { light_position.x -= 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_RIGHT]) { light_position.x += 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_Z]) { light_position.y -= 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_C]) { light_position.y += 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_RSHIFT]) { std::cout << "Pressing RSHIFT" << std::endl; }
+    if (key_state[SDL_SCANCODE_RCTRL]) { std::cout << "Pressing RCTRL" << std::endl; }
+    if (key_state[SDL_SCANCODE_W]) { camera.position.z -= 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_S]) { camera.position.z += 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_D]) { camera.position.x += 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_A]) { camera.position.x -= 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_E]) { camera.yaw += float(M_PI / 4.0) * dt; }
+    if (key_state[SDL_SCANCODE_Q]) { camera.yaw -= float(M_PI / 4.0) * dt; }
 
-	auto r = rotation(camera.pitch, camera.yaw, camera.roll);
-    camera.right   = vec3(r[0][0], r[0][1], r[0][2]);
-    camera.up      = vec3(r[1][0], r[1][1], r[1][2]);
+    auto r = rotation(camera.pitch, camera.yaw, camera.roll);
+    camera.right = vec3(r[0][0], r[0][1], r[0][2]);
+    camera.up = vec3(r[1][0], r[1][1], r[1][2]);
     camera.forward = vec3(r[2][0], r[2][1], r[2][2]);
 
     /*      c0  c1  c2
@@ -192,13 +198,14 @@ void Draw(Window& window, const Camera& camera, const vector<Triangle>& triangle
             float u = 2.0f * (float(x) / W) - 1.0f;  // Normalized between [-1, 1]
             float v = 2.0f * (float(y) / H) - 1.0f;  // Normalized between [-1, 1]
 
-            auto direction = camera.right*u*(W/2.0f)  +  camera.up*v*(H/2.0f)  +  camera.forward*camera.focal_length;
+            auto direction = camera.right * u * (W / 2.0f) + camera.up * v * (H / 2.0f) + camera.forward * camera.focal_length;
             if (ClosestIntersection(camera.position, direction, triangles, closest_intersection))
             {
                 auto triangle = triangles[closest_intersection.triangle_index];
-//                window.set_pixel(x, y, triangle.color);
-                vec3 illumination = DirectLight(triangle, closest_intersection);
-                window.set_pixel(x, y, glm::clamp(illumination, BLACK, WHITE));
+                //                window.set_pixel(x, y, triangle.color);
+                vec3 illumination = DirectLight(triangle, triangles, closest_intersection);
+                vec3 R = triangle.color * (illumination + indirectLight);
+                window.set_pixel(x, y, glm::clamp(R, BLACK, WHITE));
             }
             else
             {
@@ -222,7 +229,7 @@ bool ClosestIntersection(vec3 start, vec3 direction, const vector<Triangle>& tri
         vec3 v2 = triangle.v2;
         vec3 e1 = v1 - v0;
         vec3 e2 = v2 - v0;
-        vec3 b  = s - v0;
+        vec3 b = s - v0;
         mat3 A(-d, e1, e2);
         vec3 x = glm::inverse(A) * b;
 
@@ -231,8 +238,8 @@ bool ClosestIntersection(vec3 start, vec3 direction, const vector<Triangle>& tri
         // To get the intersection we create an if-statement
 
         float t_ray = x[0];
-        float u     = x[1];
-        float v     = x[2];
+        float u = x[1];
+        float v = x[2];
 
         if (t_ray >= 0 && u >= 0 && v >= 0 && u + v <= 1)
         {
@@ -254,15 +261,31 @@ bool ClosestIntersection(vec3 start, vec3 direction, const vector<Triangle>& tri
 }
 
 
-vec3 DirectLight(const Triangle& triangle, const Intersection& intersection) {
+vec3 DirectLight(const Triangle& triangle, vector<Triangle> triangles, const Intersection& intersection) {
     vec3  rh = light_position - intersection.position;
     float r = glm::length(rh);
     vec3  n = glm::normalize(triangle.normal);
-    float A = float(4.0 * M_PI) * r*r;
+    float A = float(4.0 * M_PI) * r * r;
     vec3  P = light_color;
     vec3  B = P / A;
-
+    //vec3 phi = vec3(0.4, 0.4, 0);
     vec3 D = B * glm::max(glm::dot(rh, n), 0.0f);
 
-    return D;
+    Intersection shadow;
+
+    if (ClosestIntersection(light_position, -rh / r, triangles, shadow)) {
+        /*std::cout << r;
+        std::cout << "hej";
+        std::cout << shadow.distance;*/
+        // These values are very close and the if statement gets true too often so if we add values to the shadow distance side we get better results somehow
+        if (shadow.distance + 0.000001 < r) {
+            //std::cout << "hej";
+            return vec3(0);
+        }
+
+    }
+
+    vec3 R = triangle.color * D;
+
+    return R;
 }
