33720045441f617608e1fb1a28813e4387c47781
tjena1 <64324604+tjena1@users.noreply.github.com>
2021-04-21 17:05:46 +0200

    Update main.cpp

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -5,6 +5,8 @@
 #include "SDL_helper.h"
 #include "TestModel.h"
 
+#include "../libraries/glm-0.9.9.8/glm/glm.hpp"
+
 
 using std::vector;
 
@@ -18,6 +20,8 @@ using glm::ivec2;
 
 const int SCREEN_WIDTH  = 500;
 const int SCREEN_HEIGHT = 500;
+vec3 cameraPos = vec3(0, 0, -3.001);
+
 
 
 // --------------------------------------------------------
@@ -25,12 +29,48 @@ const int SCREEN_HEIGHT = 500;
 
 void Draw(Window& window, const vector<Triangle>& triangles);
 void Update(float dt);
+void VertexShader(const vec3& v, ivec2& p);
+void Interpolate(ivec2 a, ivec2 b, vector<ivec2>& result);
 
+void DrawLineSDL(Window& window, ivec2 a, ivec2 b, vec3 color);
+
+void DrawPolygonEdges(Window& window, const vector<vec3>& vertices);
+
+//template <typename T>
+//T lerp(const T& start, const T& stop, float t)
+//{
+//	// t is a value between 0 - 1
+//	// t = 0 returns start
+//	// t = 1 returns stop
+//	// Otherwise, return in between
+//	return start * (1.0f - t) + stop * t;
+//}
+//
+//template <typename T>
+//std::vector<T> Interpolate(const T& start, const T& stop, int samples)
+//{
+//	std::vector<T> result;
+//	result.reserve(samples);
+//
+//	if (samples == 1) {
+//		result.push_back(start);
+//		return result;
+//	}
+//
+//	auto dividend = float(samples);
+//	for (int i = 0; i < samples; ++i) {
+//		auto value = lerp(start, stop, float(i) / dividend);
+//		result.push_back(value);
+//	}
+//
+//	return result;
+//}
+//
 
 int main(int argc, char* argv[])
 {
 	vector<Triangle> triangles = LoadTestModel();
-	Window window = Window::Create("Lab1", SCREEN_WIDTH, SCREEN_HEIGHT);
+	Window window = Window::Create("Lab3", SCREEN_WIDTH, SCREEN_HEIGHT);
 	Clock  clock  = Clock();
 
 	bool running = true;
@@ -94,19 +134,39 @@ void Draw(Window& window, const vector<Triangle>& triangles)
 
 		// Add drawing
 
-		for (int v = 0; v < 3; ++v)
+		DrawPolygonEdges(window, vertices);
+
+
+		/*for (int v = 0; v < 3; ++v)
 		{
 			ivec2 projPos;
-			/*for (int i = 0; i < 3; ++i)
-				std::cout << projPos[i];*/
+			for (int i = 0; i < 3; ++i)
+				std::cout << projPos[i];
 			VertexShader(vertices[v], projPos);
 			vec3 color(1, 1, 1);
-			std::cout << projPos.x << std::endl;
+			//std::cout << projPos.x << std::endl;
 			window.set_pixel(projPos.x, projPos.y, color);
+
 		}
+		vector<ivec2> lista(1000);
 
+		ivec2 start = ivec2(2, 100);
+		ivec2 end = ivec2(100, 100);
 
+		DrawLineSDL(window, start, end, vec3(1)); */
 	}
+}
+void Interpolate(ivec2 a, ivec2 b, vector<ivec2>& result)
+{
+	int N = result.size();
+	glm::vec2 step = glm::vec2(b - a) / float(glm::max(N - 1, 1));
+	glm::vec2 current(a);
+	for (int i = 0; i < N; ++i)
+	{
+		result[i] = current;
+		current += step;
+	}
+
 }
 
 void VertexShader(const vec3& v, ivec2& p)
@@ -117,4 +177,49 @@ void VertexShader(const vec3& v, ivec2& p)
 	auto y = int(f * P.y / P.z + (SCREEN_HEIGHT - 1) / 2.0f);
 	p = glm::clamp(ivec2(x, y), ivec2(0,0), ivec2(SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1));
 
+}
+
+
+void DrawLineSDL(Window& window, ivec2 a, ivec2 b, vec3 color) {
+
+
+	ivec2 delta = glm::abs(b - a);
+	int pixels = glm::max(delta.x, delta.y) + 1;
+	vector<ivec2> line(pixels);
+	
+	/*
+	Interpolate(a, b, line);
+	for (int i = 0; i < line.size(); ++i)
+	{
+		//std::cout << line[i].x << std::endl << line[i].y << std::endl;
+		ivec2 nya = glm::clamp(ivec2(line[i].x, line[i].y), ivec2(0, 0), ivec2(SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1));
+		window.set_pixel(nya.x, nya.y, vec3(1));
+	}*/
+
+
+	Interpolate(a, b, line);
+	for (int v = 0; v < line.size(); ++v)
+	{
+		window.set_pixel(line[v].x, line[v].y, vec3(1));
+
+	}
+
+}
+
+void DrawPolygonEdges(Window& window, const vector<vec3>& vertices)
+{
+	int V = vertices.size();
+	// Transform each vertex from 3D world position to 2D image position:
+	vector<ivec2> projectedVertices(V);
+	for (int i = 0; i < V; ++i)
+	{
+		VertexShader(vertices[i], projectedVertices[i]);
+	}
+	// Loop over all vertices and draw the edge from it to the next vertex:
+	for (int i = 0; i < V; ++i)
+	{
+		int j = (i + 1) % V; // The next vertex
+		vec3 color(1, 1, 1);
+		DrawLineSDL(window, projectedVertices[i], projectedVertices[j],color);
+	}
 }
