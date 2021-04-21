d6e905272a4e8ac44ed32670e8c74f116396e675
Ted Klein Bergman <tedber@kth.se>
2021-04-21 18:43:41 +0200

    Fixed movement

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -5,9 +5,6 @@
 #include "SDL_helper.h"
 #include "TestModel.h"
 
-#include "../libraries/glm-0.9.9.8/glm/glm.hpp"
-
-
 using std::vector;
 
 using glm::vec3;
@@ -17,11 +14,26 @@ using glm::ivec2;
 
 // ----------------------------------------------------------------------------
 // GLOBAL VARIABLES
+struct Camera {
+    vec3  position;
+
+    vec3  right;
+    vec3  up;
+    vec3  forward;
+
+    float focal_length;
+    float yaw;
+    float pitch;
+    float roll;
+};
+
+Camera camera;
+
+
 
 const int SCREEN_WIDTH  = 500;
 const int SCREEN_HEIGHT = 500;
-vec3 cameraPos = vec3(0, 0, -3.001);
-
+mat3 R;
 
 
 // --------------------------------------------------------
@@ -36,36 +48,36 @@ void DrawLineSDL(Window& window, ivec2 a, ivec2 b, vec3 color);
 
 void DrawPolygonEdges(Window& window, const vector<vec3>& vertices);
 
-//template <typename T>
-//T lerp(const T& start, const T& stop, float t)
-//{
-//	// t is a value between 0 - 1
-//	// t = 0 returns start
-//	// t = 1 returns stop
-//	// Otherwise, return in between
-//	return start * (1.0f - t) + stop * t;
-//}
-//
-//template <typename T>
-//std::vector<T> Interpolate(const T& start, const T& stop, int samples)
-//{
-//	std::vector<T> result;
-//	result.reserve(samples);
-//
-//	if (samples == 1) {
-//		result.push_back(start);
-//		return result;
-//	}
-//
-//	auto dividend = float(samples);
-//	for (int i = 0; i < samples; ++i) {
-//		auto value = lerp(start, stop, float(i) / dividend);
-//		result.push_back(value);
-//	}
-//
-//	return result;
-//}
-//
+
+mat3 rotation_x(float theta) {
+    mat3 rotation{
+            {1, 0, 0},
+            {0, cos(theta), -sin(theta)},
+            {0, sin(theta),  cos(theta)}
+    };
+    return rotation;
+}
+mat3 rotation_y(float theta) {
+    mat3 rotation{
+            { cos(theta), 0, sin(theta)},
+            {      0,     1,     0     },
+            {-sin(theta), 0, cos(theta)}
+    };
+    return rotation;
+}
+mat3 rotation_z(float theta) {
+    mat3 rotation{
+            {cos(theta), -sin(theta), 0},
+            {sin(theta),  cos(theta), 0},
+            {     0,          0,      1},
+    };
+    return rotation;
+}
+mat3 rotation(float pitch, float yaw, float roll) {
+    mat3 rotation = rotation_z(roll) * rotation_y(yaw) * rotation_x(pitch);
+    return rotation;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -73,9 +85,13 @@ int main(int argc, char* argv[])
 	Window window = Window::Create("Lab3", SCREEN_WIDTH, SCREEN_HEIGHT);
 	Clock  clock  = Clock();
 
+	camera.position = vec3(0.0, 0.0, 3.001);
+
 	bool running = true;
 	while (running)
 	{
+        float dt = clock.tick();
+
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
 		{
@@ -85,7 +101,16 @@ int main(int argc, char* argv[])
 			}
 		}
 
-		float dt = clock.tick();
+        int dx; int dy;
+        SDL_GetRelativeMouseState(&dx, &dy);
+        camera.yaw   -= float(dx) * dt;
+        camera.pitch += float(dy) * dt;
+        R = rotation(camera.pitch, camera.yaw, camera.roll);
+//        camera.right   = vec3(R[0][0], R[0][1], R[0][2]);
+//        camera.up      = vec3(R[1][0], R[1][1], R[1][2]);
+//        camera.forward = vec3(R[2][0], R[2][1], R[2][2]);
+
+
 		Update(dt);
 		Draw(window, triangles);
 
@@ -104,20 +129,16 @@ void Update(float dt)
 {
 	std::cout << "Render time: " << dt << " s." << std::endl;
 
-	const Uint8* keystate = SDL_GetKeyboardState(nullptr);
+	const Uint8* key_state = SDL_GetKeyboardState(nullptr);
 
-	if (keystate[SDL_SCANCODE_UP])     { std::cout << "Pressing UP"		<< std::endl; }
-	if (keystate[SDL_SCANCODE_DOWN])   { std::cout << "Pressing DOWN"	<< std::endl; }
-	if (keystate[SDL_SCANCODE_RIGHT])  { std::cout << "Pressing RIGHT"	<< std::endl; }
-	if (keystate[SDL_SCANCODE_LEFT])   { std::cout << "Pressing LEFT"	<< std::endl; }
-	if (keystate[SDL_SCANCODE_RSHIFT]) { std::cout << "Pressing RSHIFT" << std::endl; }
-	if (keystate[SDL_SCANCODE_RCTRL])  { std::cout << "Pressing RCTRL"	<< std::endl; }
-	if (keystate[SDL_SCANCODE_W])      { std::cout << "Pressing W"		<< std::endl; }
-	if (keystate[SDL_SCANCODE_S])      { std::cout << "Pressing S"		<< std::endl; }
-	if (keystate[SDL_SCANCODE_D])      { std::cout << "Pressing D"		<< std::endl; }
-	if (keystate[SDL_SCANCODE_A])      { std::cout << "Pressing A"		<< std::endl; }
-	if (keystate[SDL_SCANCODE_E])      { std::cout << "Pressing E"		<< std::endl; }
-	if (keystate[SDL_SCANCODE_Q])      { std::cout << "Pressing Q"		<< std::endl; }
+    if (key_state[SDL_SCANCODE_W]) { camera.position.z -= 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_S]) { camera.position.z += 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_D]) { camera.position.x += 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_A]) { camera.position.x -= 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_LSHIFT]) { camera.position.y += 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_SPACE])  { camera.position.y -= 1.0f * dt; }
+    if (key_state[SDL_SCANCODE_E]) { camera.yaw += float(M_PI / 4.0) * dt; }
+    if (key_state[SDL_SCANCODE_Q]) { camera.yaw -= float(M_PI / 4.0) * dt; }
 }
 
 void Draw(Window& window, const vector<Triangle>& triangles)
@@ -171,12 +192,11 @@ void Interpolate(ivec2 a, ivec2 b, vector<ivec2>& result)
 
 void VertexShader(const vec3& v, ivec2& p)
 {
-	vec3 P = v - cameraPos;
-	float f = SCREEN_HEIGHT; //Given in the instructions
-	auto x = int(f * P.x / P.z + (SCREEN_WIDTH - 1) / 2.0f);
+	float f = -SCREEN_HEIGHT; //Given in the instructions
+	vec3 P = R * (v - camera.position);
+	auto x = int(f * P.x / P.z + (SCREEN_WIDTH  - 1) / 2.0f);
 	auto y = int(f * P.y / P.z + (SCREEN_HEIGHT - 1) / 2.0f);
 	p = glm::clamp(ivec2(x, y), ivec2(0,0), ivec2(SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1));
-
 }
 
 
