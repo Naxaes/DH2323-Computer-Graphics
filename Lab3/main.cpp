f8ca30c5b588fc52807f2ac1c3fe8f209ee50327
Ted Klein Bergman <tedber@kth.se>
2021-04-26 19:40:05 +0200

    Fixed rasterizer

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
 
 #include "glm/glm.hpp"
 #include "SDL_helper.h"
 #include "TestModel.h"
 #include <algorithm>
 
+
 using std::vector;
+using std::tuple;
 
 using glm::vec3;
 using glm::mat3;
 using glm::ivec2;
 
 
-// ----------------------------------------------------------------------------
-// GLOBAL VARIABLES
+const int SCREEN_WIDTH  = 500;
+const int SCREEN_HEIGHT = 500;
+
+
+struct Pixel {
+    int x;
+    int y;
+    float z_inv;
+};
+
+
 struct Camera {
 	vec3  position;
 
@@ -22,31 +34,30 @@ struct Camera {
 	vec3  up;
 	vec3  forward;
 
+    mat3  transform;
+
 	float focal_length;
 	float yaw;
 	float pitch;
 	float roll;
+
 };
 
-Camera camera;
-
-
-
-const int SCREEN_WIDTH = 500;
-const int SCREEN_HEIGHT = 500;
-mat3 R;
+float depth_buffer[SCREEN_HEIGHT][SCREEN_WIDTH] = { 0 };
 
 
 // --------------------------------------------------------
 // FUNCTION DECLARATIONS
 
-void Draw(Window& window, const vector<Triangle>& triangles);
-void Update(float dt);
-void VertexShader(const vec3& v, ivec2& p);
-void Interpolate(ivec2 a, ivec2 b, vector<ivec2>& result);
-void DrawLineSDL(Window& window, ivec2 a, ivec2 b, vec3 color);
+void Draw(Window& window, const Camera& camera, const vector<Triangle>& triangles);
+void Update(Camera& camera, float dt);
+
+Pixel VertexShader(const Camera& camera, const vec3& v);
+vector<Pixel> Interpolate(Pixel a, Pixel b);
+
+void DrawLine(Window& window, Pixel a, Pixel b, vec3 color);
 void DrawPolygonEdges(Window& window, const vector<vec3>& vertices);
-void ComputePolygonRows(const vector<ivec2>& vertexPixels, vector<ivec2>& leftPixels, vector<ivec2>& rightPixels);
+tuple<vector<Pixel>, vector<Pixel>> ComputePolygonRows(const vector<Pixel>& vertexPixels);
 
 mat3 rotation_x(float theta) {
 	mat3 rotation{
@@ -82,8 +93,9 @@ int main(int argc, char* argv[])
 {
 	vector<Triangle> triangles = LoadTestModel();
 	Window window = Window::Create("Lab3", SCREEN_WIDTH, SCREEN_HEIGHT);
-	Clock  clock = Clock();
+	Clock  clock  = Clock();
 
+    Camera camera = { };
 	camera.position = vec3(0.0, 0.0, 3.001);
 
 	bool running = true;
@@ -100,17 +112,8 @@ int main(int argc, char* argv[])
 			}
 		}
 
-		int dx; int dy;
-		SDL_GetRelativeMouseState(&dx, &dy);
-		camera.yaw -= float(dx) * dt;
-		camera.pitch += float(dy) * dt;
-		R = rotation(camera.pitch, camera.yaw, camera.roll);
-		//        camera.right   = vec3(R[0][0], R[0][1], R[0][2]);
-		//        camera.up      = vec3(R[1][0], R[1][1], R[1][2]);
-		//        camera.forward = vec3(R[2][0], R[2][1], R[2][2]);
-
-		Update(dt);
-		Draw(window, triangles);
+		Update(camera, dt);
+		Draw(window, camera, triangles);
 
 		// NOTE: The pixels are not shown on the screen 
 		// until we update the window with this method.
@@ -123,9 +126,9 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-void Update(float dt)
+void Update(Camera& camera, float dt)
 {
-	//std::cout << "Render time: " << dt << " s." << std::endl;
+//	std::cout << "Render time: " << dt << " s." << std::endl;
 
 	const Uint8* key_state = SDL_GetKeyboardState(nullptr);
 
@@ -134,180 +137,167 @@ void Update(float dt)
 	if (key_state[SDL_SCANCODE_D]) { camera.position.x += 1.0f * dt; }
 	if (key_state[SDL_SCANCODE_A]) { camera.position.x -= 1.0f * dt; }
 	if (key_state[SDL_SCANCODE_LSHIFT]) { camera.position.y += 1.0f * dt; }
-	if (key_state[SDL_SCANCODE_SPACE]) { camera.position.y -= 1.0f * dt; }
+	if (key_state[SDL_SCANCODE_SPACE])  { camera.position.y -= 1.0f * dt; }
 	if (key_state[SDL_SCANCODE_E]) { camera.yaw += float(M_PI / 4.0) * dt; }
 	if (key_state[SDL_SCANCODE_Q]) { camera.yaw -= float(M_PI / 4.0) * dt; }
+
+    int dx; int dy;
+    SDL_GetRelativeMouseState(&dx, &dy);
+    camera.yaw   -= float(dx) * dt * 0.1f;
+    camera.pitch += float(dy) * dt * 0.1f;
+    camera.transform = rotation(camera.pitch, camera.yaw, camera.roll);
 }
 
-void Draw(Window& window, const vector<Triangle>& triangles)
+vector<Pixel> Rasterize(const vector<Pixel>& polygon)
+{
+    auto[left, right] = ComputePolygonRows(polygon);
+    vector<Pixel> pixels;
+    pixels.reserve(left.size());
+    for (int row = 0; row < left.size(); ++row)
+        for (const Pixel& pixel : Interpolate(left[row], right[row]))
+            if (depth_buffer[pixel.y][pixel.x] < pixel.z_inv) {
+                depth_buffer[pixel.y][pixel.x] = pixel.z_inv;
+                pixels.push_back(pixel);
+            }
+
+    return pixels;
+}
+
+
+void Draw(Window& window, const Camera& camera, const vector<Triangle>& triangles)
 {
 	window.fill(BLACK);
 
+    for (int y = 0; y < SCREEN_HEIGHT; ++y)
+        for (int x = 0; x < SCREEN_WIDTH; ++x)
+            depth_buffer[y][x] = 0;
+
 	for (const auto& triangle : triangles)
 	{
-		vector<vec3> vertices(3);
+		vector<Pixel> polygon = {
+		        VertexShader(camera, triangle.v0),
+		        VertexShader(camera, triangle.v1),
+		        VertexShader(camera, triangle.v2),
+		};
 
-		vertices[0] = triangle.v0;
-		vertices[1] = triangle.v1;
-		vertices[2] = triangle.v2;
+        vector<Pixel> pixels = Rasterize(polygon);
 
-		// Add drawing
-
-		DrawPolygonEdges(window, vertices);
-		vector<ivec2> vertexPixels(3);
-		vertexPixels[0] = ivec2(10, 5);
-		vertexPixels[1] = ivec2(5, 10);
-		vertexPixels[2] = ivec2(15, 15);
-		vector<ivec2> leftPixels;
-		vector<ivec2> rightPixels;
-		ComputePolygonRows(vertexPixels, leftPixels, rightPixels);
-		for (int row = 0; row < leftPixels.size(); ++row)
-		{
-			std::cout << "Start: ("
-				<< leftPixels[row].x << ","
-				<< leftPixels[row].y << "). "
-				<< "End: ("
-				<< rightPixels[row].x << ","
-				<< rightPixels[row].y << "). " << std::endl;
-		}
-
-		/*for (int v = 0; v < 3; ++v)
-		{
-			ivec2 projPos;
-			for (int i = 0; i < 3; ++i)
-				std::cout << projPos[i];
-			VertexShader(vertices[v], projPos);
-			vec3 color(1, 1, 1);
-			//std::cout << projPos.x << std::endl;
-			window.set_pixel(projPos.x, projPos.y, color);
-		}
-		vector<ivec2> lista(1000);
-		ivec2 start = ivec2(2, 100);
-		ivec2 end = ivec2(100, 100);
-		DrawLineSDL(window, start, end, vec3(1)); */
+        for (const Pixel& pixel : pixels)
+            window.set_pixel(pixel.x, pixel.y, triangle.color);
 	}
 }
-void Interpolate(ivec2 a, ivec2 b, vector<ivec2>& result)
+vector<Pixel> Interpolate(Pixel a, Pixel b)
 {
-	int N = result.size();
-	glm::vec2 step = glm::vec2(b - a) / float(glm::max(N - 1, 1));
-	glm::vec2 current(a);
-	for (int i = 0; i < N; ++i)
+    vec3 start(a.x, a.y, a.z_inv);
+    vec3 stop(b.x, b.y, b.z_inv);
+
+    auto delta = glm::abs(stop - start);
+    int pixels = int(glm::max(delta.x, delta.y)) + 1;
+    vector<Pixel> line(pixels);
+
+	auto step = vec3(stop - start) / float(glm::max(pixels - 1, 1));
+	vec3 current(start);
+	for (int i = 0; i < pixels; ++i)
 	{
-		result[i] = current;
+        line[i] = Pixel { int(current.x), int(current.y), current.z };
 		current += step;
 	}
 
+    return line;
 }
 
-void VertexShader(const vec3& v, ivec2& p)
+Pixel VertexShader(const Camera& camera, const vec3& v)
 {
 	float f = -SCREEN_HEIGHT; //Given in the instructions
-	vec3 P = R * (v - camera.position);
+	vec3 P = camera.transform * (v - camera.position);
 	auto x = int(f * P.x / P.z + (SCREEN_WIDTH - 1) / 2.0f);
 	auto y = int(f * P.y / P.z + (SCREEN_HEIGHT - 1) / 2.0f);
-	p = glm::clamp(ivec2(x, y), ivec2(0, 0), ivec2(SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1));
+
+	auto result = glm::clamp(ivec2(x, y), ivec2(0, 0), ivec2(SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1));
+	return { result.x, result.y, -1.0f / P.z };
 }
 
 
-void DrawLineSDL(Window& window, ivec2 a, ivec2 b, vec3 color) {
-
-
-	ivec2 delta = glm::abs(b - a);
-	int pixels = glm::max(delta.x, delta.y) + 1;
-	vector<ivec2> line(pixels);
-
-	/*
-	Interpolate(a, b, line);
-	for (int i = 0; i < line.size(); ++i)
+void DrawLine(Window& window, Pixel a, Pixel b, vec3 color)
+{
+	auto line = Interpolate(a, b);
+	for (auto & v : line)
 	{
-		//std::cout << line[i].x << std::endl << line[i].y << std::endl;
-		ivec2 nya = glm::clamp(ivec2(line[i].x, line[i].y), ivec2(0, 0), ivec2(SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1));
-		window.set_pixel(nya.x, nya.y, vec3(1));
-	}*/
-
-
-	Interpolate(a, b, line);
-	for (int v = 0; v < line.size(); ++v)
-	{
-		window.set_pixel(line[v].x, line[v].y, vec3(1));
-
+	    if (depth_buffer[v.y][v.x] < v.z_inv)
+	    {
+            depth_buffer[v.y][v.x] = v.z_inv;
+	        window.set_pixel(v.x, v.y, color);
+	    }
 	}
-
 }
 
-void DrawPolygonEdges(Window& window, const vector<vec3>& vertices)
+void DrawPolygonEdges(Window& window, const Camera& camera, const vector<vec3>& vertices)
 {
 	int V = vertices.size();
 	// Transform each vertex from 3D world position to 2D image position:
-	vector<ivec2> projectedVertices(V);
+	vector<Pixel> projectedVertices(V);
 	for (int i = 0; i < V; ++i)
 	{
-		VertexShader(vertices[i], projectedVertices[i]);
+        projectedVertices[i] = VertexShader(camera, vertices[i]);
 	}
+
 	// Loop over all vertices and draw the edge from it to the next vertex:
 	for (int i = 0; i < V; ++i)
 	{
 		int j = (i + 1) % V; // The next vertex
-		vec3 color(1, 1, 1);
-		DrawLineSDL(window, projectedVertices[i], projectedVertices[j], color);
+		DrawLine(window, projectedVertices[i], projectedVertices[j], vec3(1, 1, 1));
 	}
 }
-void ComputePolygonRows(const vector<ivec2>& vertexPixels, vector<ivec2>& leftPixels, vector<ivec2>& rightPixels)
+
+tuple<vector<Pixel>, vector<Pixel>> ComputePolygonRows(const vector<Pixel>& vertexPixels)
 {
-	vector<float> yvalues;
-	int numberOfrows;
-	int max;
-	int min;
-	for (int i = 0; i < vertexPixels.size(); ++i)
+    const int vertexCount = vertexPixels.size();
+
+	vector<float> yvalues(vertexCount);
+	for (int i = 0; i < vertexCount; ++i)
+		yvalues[i] = vertexPixels[i].y;
+
+
+	// Calculates max and min.
+	auto max = std::max_element(yvalues.begin(), yvalues.end());
+	auto min = std::min_element(yvalues.begin(), yvalues.end());
+
+//	std::cout << "Y-values: " << *min << " - " << *max << std::endl;
+
+	// Calculates number of rows according to the given formulas.
+	auto numberOfRows = int(*max - *min) + 1;
+
+//	std::cout << numberOfRows << std::endl;
+
+    vector<Pixel> left(numberOfRows);
+    vector<Pixel> right(numberOfRows);
+
+	for (int i = 0; i < numberOfRows; ++i)
 	{
-		yvalues.push_back(vertexPixels[i].y);
+        left[i].x  = std::numeric_limits<int>::max();
+        right[i].x = std::numeric_limits<int>::min();
 	}
 
-	
-	//Caluclates max and min
-	max = *std::max_element(yvalues.begin(), yvalues.end());
-	min = *std::min_element(yvalues.begin(), yvalues.end());
-
-	//std::cout << yvalues[i] << std::endl;
-
-	//Caluclates number of rows according to the given formulas
-	numberOfrows = max - min + 1;
-
-	std::cout << numberOfrows << std::endl;
-	leftPixels.resize(numberOfrows);
-	rightPixels.resize(numberOfrows);
-
-	for (int i = 0; i < numberOfrows; ++i)
+	for (int i = 0; i < vertexCount; ++i)
 	{
-		leftPixels[i].x = +std::numeric_limits<int>::max();
-		rightPixels[i].x = -std::numeric_limits<int>::max();
+        int j = (i + 1) % vertexCount;
+
+		auto v0 = vertexPixels[i];
+		auto v1 = vertexPixels[j];
+
+		auto line = Interpolate(v0, v1);
+
+		for (const auto v : line) {
+
+		    int row = int(v.y - *min + 0.5f);
+
+		    auto& a = left[row];
+            auto& b = right[row];
+
+            if (a.x > v.x) { a = v; }
+            if (b.x < v.x) { b = v; }
+		}
 	}
 
-	vector<ivec2> number(numberOfrows);
-
-
-	for (int i = 0; i < vertexPixels.size(); ++i)
-	{
-		ivec2 firstedge = vertexPixels[i];
-		ivec2 end = vertexPixels[i+1];
-		Interpolate(firstedge, end, number);
-		std::cout << number[i].x;
-
-	}
-	
-	//std::max_element(vertexPixels);
-	// 1. Find max and min y-value of the polygon
-	// and compute the number of rows it occupies.
-	// 2. Resize leftPixels and rightPixels
-	// so that they have an element for each row.
-	// 3. Initialize the x-coordinates in leftPixels
-	// to some really large value and the x-coordinates
-	// in rightPixels to some really small value.
-	// 4. Loop through all edges of the polygon and use
-	// linear interpolation to find the x-coordinate for
-	// each row it occupies. Update the corresponding
-	// values in rightPixels and leftPixels.
-
-
+	return { left, right };
 }
